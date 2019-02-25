c9a4ce78d0f352fab9f9e9a76a43142cf392b342
oXis <oxis44@gmail.com>
2019-02-25 13:03:00 +0000

    add slexy

--- phparsers.py
+++ phparsers.py
@@ -1,16 +1,19 @@
 import logging
 
 from pastebinParser import PastebinParser
+from parsers.slexyParser import SlexyParser
 
 
 class PhParser():
 
     def __init__(self):
         logging.info("Init parsers")
-        self.parsers = [PastebinParser(
-                        "https://scrape.pastebin.com/api_scraping.php?limit=50",
-                        10,
-                        "Pastebin")
+        self.parsers = [PastebinParser("https://scrape.pastebin.com/api_scraping.php?limit=100",
+                                       120,
+                                       "Pastebin"),
+                        SlexyParser("https://slexy.org/recent",
+                                    120,
+                                    "Slexy")
                         ]
 
     def run(self):
@@ -18,7 +21,11 @@ class PhParser():
         rets = []
 
         for parser in self.parsers:
-            ret = parser.run()
+            try:
+                ret = parser.run()
+            except Exception as e:
+                logging.error("Parser %s crashed, %s", parser.name, e)
+                ret = None
             if ret:
                 rets.append(ret)
 
